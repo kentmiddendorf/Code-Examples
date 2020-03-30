3fc560748533e8a7399d2bd5a082487fd38c16d9
kentmiddendorf <62567851+kentmiddendorf@users.noreply.github.com>
2020-03-30 13:43:17 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -201,3 +201,32 @@ public class Library {
         return null;
     }
 }
+
+public class Album {
+
+    private String albumName;
+    private ArrayList<Song> songs;
+
+    public Album(String albumName) {
+        this.albumName = albumName;
+        this.songs = new ArrayList<Song>();
+    }
+
+    public String getAlbumName() {
+        return albumName;
+    }
+
+    public void addSong (String songName, int length) {
+        this.songs.add(new Song(songName, length));
+    }
+
+    public Song findSong (String songName) {
+        for (int i = 0; i < this.songs.size(); i ++) {
+            Song foundSong = this.songs.get(i);
+            if (foundSong.getName().equals(songName)) {
+                return foundSong;
+            }
+        }
+        return null;
+    }
+}
