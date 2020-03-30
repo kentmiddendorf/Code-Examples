fd8cf3f816fae8ee6e35d624c91f56f757230d9d
kentmiddendorf <62567851+kentmiddendorf@users.noreply.github.com>
2020-03-30 13:44:00 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -230,3 +230,19 @@ public class Album {
         return null;
     }
 }
+
+public class Song {
+
+    private String name;
+    private int length;
+
+    public Song(String name, int length) {
+        this.name = name;
+        this.length = length;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
